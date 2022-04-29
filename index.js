d0a77da49d3e14f48bca2e28dbda6791f8c306d5
Fernando de Lafuente <fernando.delafuenteq@gmail.com>
2022-04-28 21:15:47 -0500

    Paso 7: Creando la clase FizzbuzzService

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const Reader = require("../fizzbuz/lib/utils/Reader")
 const ExplorerService = require("../fizzbuz/lib/services/ExplorerService")
+const FizzbuzzService = require("../fizzbuz/lib/services/FizzbuzzService")
+
 
 // ----> Reader
 const explorers = Reader.readJsonFile("explorers.json")
@@ -8,8 +10,20 @@ const explorers = Reader.readJsonFile("explorers.json")
 
 // ----> ExplorerService
 // console.log(ExplorerService.filterByMission(explorers, "node"))
-
-
-
 // console.log(ExplorerService.getAmountOfExplorersByMission(explorers, "node"))
-console.log(ExplorerService.getExplorersUsernamesByMission(explorers, "node"))
+// console.log(ExplorerService.getExplorersUsernamesByMission(explorers, "node"))
+
+
+// ----> FizzbuzzService
+
+const explorer1 = {name: "Explorer1", score: 1}
+console.log(FizzbuzzService.applyValidationInExplorer(explorer1)) // {name: "Explorer1", score: 1, trick: 1} 
+
+const explorer3 = {name: "Explorer3", score: 3}
+console.log(FizzbuzzService.applyValidationInExplorer(explorer3)) // {name: "Explorer3", score: 3, trick: "FIZZ"}
+
+const explorer5 = {name: "Explorer5", score: 5}
+console.log(FizzbuzzService.applyValidationInExplorer(explorer5)) // {name: "Explorer5", score: 5, trick: "BUZZ"}
+
+const explorer15 = {name: "Explorer15", score: 15}
+console.log(FizzbuzzService.applyValidationInExplorer(explorer15)) // {name: "Explorer15", score: 15, trick: "FIZZBUZZ"}
