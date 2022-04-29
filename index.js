fa837309d7f6136b59251cc0309c59a53bf8fd93
Fernando de Lafuente <fernando.delafuenteq@gmail.com>
2022-04-28 21:04:05 -0500

    Paso 6: Creando la clase ExplorerService

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 const Reader = require("../fizzbuz/lib/utils/Reader")
+const ExplorerService = require("../fizzbuz/lib/services/ExplorerService")
 
+// ----> Reader
 const explorers = Reader.readJsonFile("explorers.json")
+// console.log(explorers)
 
-console.log(explorers)
+
+// ----> ExplorerService
+// console.log(ExplorerService.filterByMission(explorers, "node"))
+
+
+
+// console.log(ExplorerService.getAmountOfExplorersByMission(explorers, "node"))
+console.log(ExplorerService.getExplorersUsernamesByMission(explorers, "node"))
