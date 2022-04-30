9673618ea5f00eb8798918a63f79469d432d77b8
Fernando de Lafuente <fernando.delafuenteq@gmail.com>
2022-04-30 18:15:04 -0500

    Paso 18: Usando Linter to Fix the files

--- test/controllers/ExplorerController.test.js
+++ test/controllers/ExplorerController.test.js
@@ -1,17 +1,17 @@
-const ExplorerController = require("../../lib/controllers/ExplorerController")
+const ExplorerController = require("../../lib/controllers/ExplorerController");
 
 describe("Test para ExplorerController", () => {
     test("Prueba 1: Explorers by mission", () => {
-        const testingExplorersByMission = ExplorerController.getExplorersByMission("node")
-        const explorer1 = testingExplorersByMission[0].name
-        expect(explorer1).toBe("Woopa1")
-    })
+        const testingExplorersByMission = ExplorerController.getExplorersByMission("node");
+        const explorer1 = testingExplorersByMission[0].name;
+        expect(explorer1).toBe("Woopa1");
+    });
     test("Prueba 2: Explorers Usernames by mission", () => {
-        const testingExplorersUsernameByMission = ExplorerController.getExplorersUsernamesByMission("node")
-        expect(testingExplorersUsernameByMission).toStrictEqual(["ajolonauta1", "ajolonauta2", "ajolonauta3", "ajolonauta4", "ajolonauta5", "ajolonauta11", "ajolonauta12", "ajolonauta13", "ajolonauta14", "ajolonauta15"])
-    })
+        const testingExplorersUsernameByMission = ExplorerController.getExplorersUsernamesByMission("node");
+        expect(testingExplorersUsernameByMission).toStrictEqual(["ajolonauta1", "ajolonauta2", "ajolonauta3", "ajolonauta4", "ajolonauta5", "ajolonauta11", "ajolonauta12", "ajolonauta13", "ajolonauta14", "ajolonauta15"]);
+    });
     test("Prueba 3: Amount explorers by mission", () => {
-        const testingAmountExplorersByMission = ExplorerController.getExplorersAmountByMission("node")
-        expect(testingAmountExplorersByMission).toBe(10)
-    })
-})
+        const testingAmountExplorersByMission = ExplorerController.getExplorersAmountByMission("node");
+        expect(testingAmountExplorersByMission).toBe(10);
+    });
+});
