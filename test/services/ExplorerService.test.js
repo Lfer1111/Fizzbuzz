0ec2a575d06ef9b806a5ed5039baa58657d4e0ae
Fernando de Lafuente <fernando.delafuenteq@gmail.com>
2022-04-29 19:10:32 -0500

    Paso 14: Agregando ESLint Linter como dependencia and fix all the files

--- test/services/ExplorerService.test.js
+++ test/services/ExplorerService.test.js
@@ -1,17 +1,17 @@
-const Reader = require("../../lib/utils/Reader")
-const ExplorerService = require("../../lib/services/ExplorerService")
+const Reader = require("../../lib/utils/Reader");
+const ExplorerService = require("../../lib/services/ExplorerService");
 
 describe("Test para ExplorerService", () => {
     test("1. Calcular todos los explorers con Node", () => {
-        const explorers = Reader.readJsonFile("explorers.json")
-        const explorersInNode = ExplorerService.filterByMission(explorers, "node")
-        expect(explorersInNode.length).toBe(10)
-    })
+        const explorers = Reader.readJsonFile("explorers.json");
+        const explorersInNode = ExplorerService.filterByMission(explorers, "node");
+        expect(explorersInNode.length).toBe(10);
+    });
 
     test("2. Calcular todos los explorers con Java", () => {
-        const explorers = Reader.readJsonFile("explorers.json")
-        const explorersInNode = ExplorerService.filterByMission(explorers, "java")
-        expect(explorersInNode.length).toBe(5)
-    })
+        const explorers = Reader.readJsonFile("explorers.json");
+        const explorersInNode = ExplorerService.filterByMission(explorers, "java");
+        expect(explorersInNode.length).toBe(5);
+    });
 
-})
+});
